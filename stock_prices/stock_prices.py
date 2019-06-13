7a7232075eecbb0a9d8313a24e4cf1705ddbea27
Estevan <estevan.rodriguez10@yahoo.com>
2019-06-12 23:43:12 -0700

    finished stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,16 +3,45 @@
 import argparse
 
 def find_max_profit(prices):
-  lowest_price_so_far = prices[0]
-  max_profit = 0
+  #firs-pass attempt (incorrect)
   
-  for price in prices: 
-    if price <= lowest_price_so_far:
-      lowest_price_so_far = price
+  # lowest_price_so_far = prices[0]
+  # max_price_so_far = prices[1]
+  # max_profit = prices[1] - prices[0]
+  
+  # for price in prices[:len(prices)-1]: 
+  #   if price < lowest_price_so_far and price < max_price_so_far:
+  #     lowest_price_so_far = price
+  #     max_profit = max_price_so_far - lowest_price_so_far
+  #     print("p",max_profit)
+  #     print("l", lowest_price_so_far)
     
-    for next_price in prices[1:len(prices) -1]:
-      if max_profit < next_price - lowest_price_so_far:
-        max_profit = next_price - lowest_price_so_far
+  #   for next_price in prices[1:len(prices)]:
+  #     if next_price > max_price_so_far:
+  #       max_price_so_far = next_price
+  #       print("m",max_price_so_far)
+  #       max_profit = max_price_so_far - lowest_price_so_far
+  #       print("p",max_profit)
+  max_price = prices[1]
+  max_profit = prices[1] - prices[0]
+  
+  if len(prices) == 1: #edge case
+    return prices[0]
+  
+  elif len(prices) == 0: #empty array
+    return
+  
+  else:
+    
+    for i in range(1,len(prices)):
+      max_price = prices[i]
+      
+      
+      for j in range(i):
+        if max_profit < max_price - prices[j]:
+          max_profit = max_price - prices[j]
+
+
 
   return max_profit
 
